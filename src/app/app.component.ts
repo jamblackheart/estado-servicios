993b78e34e0b0b2a58018b584cf7c3ee5f33c371
jairo marin <jairoa.marinm@gmail.com>
2019-08-21 11:13:11 -0500

    jam

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import {SPServicio} from './servicios/sp-servicio';
 import {Servicios} from './dominios/servicios';
-
+import {Usuario} from './dominios/usuario';
+import {Grupo} from './dominios/grupo';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,14 @@ export class AppComponent{
   listaServicios: Servicios[] = [];
   totalServicios: number = 0;
   totalIncidentes: number = 0;
+
+
+  nombreUsuario: any;
+  emailUsuario: any;
+  idUsuario: number;
+  usuarioActual: Usuario;
+  esColaborador: boolean;
+  grupos: Grupo[] = []; 
   
 
 
@@ -21,10 +30,49 @@ export class AppComponent{
 
   ngOnInit() {
    
-    
+    this.ObtenerUsuarioActual() ;
     this.ObtenerTodosLosServicios();
 
   }
+
+  ObtenerUsuarioActual() {
+    this.servicios.ObtenerUsuarioActual().subscribe(
+      (respuesta) => {
+        this.usuarioActual = new Usuario(respuesta.Title, respuesta.email, respuesta.Id);
+        this.nombreUsuario = this.usuarioActual.nombre;
+        this.idUsuario = this.usuarioActual.id;
+        
+        console.log(this.nombreUsuario);
+        this.obtenerGrupos();
+
+      }, err => {
+        console.log('Error obteniendo usuario: ' + err);
+        
+      }
+    )
+  };
+
+
+  obtenerGrupos() {
+    let idUsuario = this.usuarioActual.id;
+    this.servicios.ObtenerGruposUsuario(idUsuario).subscribe(
+    (respuesta) => {
+    this.grupos = Grupo.fromJsonList(respuesta);
+    this.verificarPermisos();
+   
+    }, err => {
+    console.log('Error obteniendo grupos de usuario: ' + err);
+    }
+    )
+    };
+    
+    verificarPermisos() {
+    let existeEsColaborador = this.grupos.find(x => x.title === "Colaboradores");
+    if (existeEsColaborador !== undefined) {
+      this.esColaborador= true;
+    };
+    }; 
+
 
   ObtenerTodosLosServicios(){
     this.servicios.ObtenerTodosLosServicios().then(
